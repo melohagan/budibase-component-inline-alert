0fdcc2e86e067f26504dd4a604f7df4dc466a70a
Mel O'Hagan <mel@budibase.com>
2023-01-06 17:37:18 +0000

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spectrum-inline-alert
+A container for other Budibase components. It's appearance is determined by the chosen variant: https://opensource.adobe.com/spectrum-css/inlinealert.html
+
+# Description
+In-line alerts display a non-modal message associated with objects in a view. These are often used in form validation, providing a place to aggregate feedback related to multiple fields.
+
+Find out more about [Budibase](https://github.com/Budibase/budibase).
+
+
---
+++ src/Component.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { getContext } from "svelte"
+
+  export let title
+  export let variant
+
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
+
+  const iconMap = {
+    info: "Info",
+    positive: "CheckmarkCircle",
+    negative: "Alert",
+    notice: "Alert",
+  }
+  $: icon = iconMap[variant]
+</script>
+
+<div use:styleable={$component.styles}>
+  <div class="spectrum-InLineAlert spectrum-InLineAlert--info">
+    <div class="spectrum-InLineAlert-header">
+        {title}
+      <svg class="spectrum-Icon spectrum-Icon--sizeM spectrum-InLineAlert-icon" focusable="false" aria-hidden="true">
+        <use xlink:href="#spectrum-icon-18-{icon}" />
+      </svg>
+    </div>
+    <div class="spectrum-InLineAlert-content">
+      <slot />
+    </div>
+  </div>
+</div>
